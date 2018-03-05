d931a1664a417c97f73aad542756296f58a74963
Dolomaniuk <dalamas200487@gmail.com>
2018-03-06 00:05:45 +0300

    +2 решенные задачи

--- week_2/Boxes/solution.py
+++ week_2/Boxes/solution.py
@@ -1,3 +1,9 @@
+'''
+Есть две коробки, первая размером A₁×B₁×C₁, вторая размером A₂×B₂×C₂.
+Определите, можно ли разместить одну из этих коробок внутри другой, при условии,
+что поворачивать коробки можно только на 90 градусов вокруг ребер
+'''
+
 a1 = int(input())
 b1 = int(input())
 c1 = int(input())
@@ -6,12 +12,13 @@ b2 = int(input())
 c2 = int(input())
 
 
-def sorting(a, b, c):
+def sorting(a, b, c):       # сортируем в порядке убывания
     while (a < b) or (b < c):
         if a < b:
             a, b = b, a
         if b < c:
             b, c = c, b
+    # print(a, b, c)
     return a, b, c
 
 
@@ -23,6 +30,12 @@ if a1 == a2 and b1 == b2 and c1 == c2:
 elif a1 >= a2:
     if b1 >= b2 and c1 >= c2:
         print('The first box is larger than the second one')
-else:
+    else:
+        print('Boxes are incomparable')
+elif a1 <= a2:
     if b1 <= b2 and c1 <= c2:
         print('The first box is smaller than the second one')
+    else:
+        print('Boxes are incomparable')
+else:
+    print('Boxes are incomparable')
---
+++ week_2/Пробежка/solution.py
@@ -0,0 +1,13 @@
+"""
+Пробежка
+"""
+x = float(input())
+y = float(input())
+
+day = 1
+way = x
+
+while way <= y:
+    day += 1
+    way = way + ((way * 10) / 100)
+print(day)
