73e91eac694f9c385c1fb2c1fbe9d98ea82ec955
Dolomaniuk <dalamas200487@gmail.com>
2018-03-08 20:07:38 +0300

    обновлено

--- week_2/Второй максимум/solution.py
+++ week_2/Второй максимум/solution.py
@@ -2,13 +2,15 @@
 Определить максимум в последовательности натуральных чисел, конец = 0
 """
 n = int(input())
-max = n
+max1 = n
+max2 = n
 
-while n != 0 and n < (10 ** 9):
+while n != 0:
     n = int(input())
-    if n == 0:
-        break
-    if n > max:
-        max = n
-
-print(max)
+    if n > max1:
+        max1 = n
+        # print('max1:', max1)
+    if max2 < n < max1:     #вычисляем второй максимум
+        max2 = n
+        # print('max2:', max2)
+print(max2)
