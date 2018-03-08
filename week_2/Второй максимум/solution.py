b4265788af1b8b2eb46d66a4a3a0057eb00aa09f
Dolomaniuk <dalamas200487@gmail.com>
2018-03-08 20:45:54 +0300

    обновлено

--- week_2/Второй максимум/solution.py
+++ week_2/Второй максимум/solution.py
@@ -7,10 +7,10 @@ max2 = n
 
 while n != 0:
     n = int(input())
-    if n > max1:
+    if n > max1 and max1 > max2:
         max1 = n
-        # print('max1:', max1)
+        print('max1:', max1)
     if max2 < n < max1:     #вычисляем второй максимум
         max2 = n
-        # print('max2:', max2)
+        print('max2:', max2)
 print(max2)
