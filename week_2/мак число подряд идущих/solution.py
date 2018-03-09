87aeff8c0a1693d4039cb82490d3d010b4cdf5dc
Dolomaniuk <dalamas200487@gmail.com>
2018-03-09 21:47:10 +0300

    100/100

--- week_2/мак число подряд идущих/solution.py
+++ week_2/мак число подряд идущих/solution.py
@@ -7,11 +7,11 @@ while n != 0:
     n = int(input())
     if n == prevNumber:
         i += 1
+        if i > count:
+            count = i
     elif n == 0:
         continue
     else:
-        if i > count:
-            count = i
         i = 1
     prevNumber = n
 print(count)
