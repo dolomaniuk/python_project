118482f37c4754cb7f212e45ae453290be0d6b89
Dolomaniuk <dalamas200487@gmail.com>
2018-03-09 17:36:30 +0300

    решено 100/100

--- week_2/Второй максимум/solution.py
+++ week_2/Второй максимум/solution.py
@@ -1,16 +1,17 @@
 """
 Определить максимум в последовательности натуральных чисел, конец = 0
 """
-n = int(input())
-max1 = n
-max2 = n
-
+# n = int(input())
+max1 = 0
+max2 = 0
+n = -1
 while n != 0:
     n = int(input())
-    if n > max1 and max1 > max2:
+    if n > max1:
+        max2 = max1
+        # print('max2', max2)
         max1 = n
-        print('max1:', max1)
-    if max2 < n < max1:     #вычисляем второй максимум
+        # print('max1:', max1)
+    elif n > max2:
         max2 = n
-        print('max2:', max2)
 print(max2)
