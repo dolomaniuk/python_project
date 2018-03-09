324ec07bb2db45ae92b9d41fa2308d25cf57451b
Dolomaniuk <dalamas200487@gmail.com>
2018-03-09 22:40:19 +0300

    9/100

--- week_2/Boxes/solution.py
+++ week_2/Boxes/solution.py
@@ -1,9 +1,3 @@
-'''
-Есть две коробки, первая размером A₁×B₁×C₁, вторая размером A₂×B₂×C₂.
-Определите, можно ли разместить одну из этих коробок внутри другой, при условии,
-что поворачивать коробки можно только на 90 градусов вокруг ребер
-'''
-
 a1 = int(input())
 b1 = int(input())
 c1 = int(input())
@@ -12,7 +6,7 @@ b2 = int(input())
 c2 = int(input())
 
 
-def sorting(a, b, c):       # сортируем в порядке убывания
+def sorting(a, b, c):
     while (a < b) or (b < c):
         if a < b:
             a, b = b, a
@@ -26,15 +20,11 @@ a2, b2, c2 = sorting(a2, b2, c2)
 
 if a1 == a2 and b1 == b2 and c1 == c2:
     print('Boxes are equal')
-elif a1 >= a2:
-    if b1 >= b2 and c1 >= c2:
-        print('The first box is larger than the second one')
-    else:
-        print('Boxes are incomparable')
-elif a1 <= a2:
-    if b1 <= b2 and c1 <= c2:
-        print('The first box is smaller than the second one')
-    else:
-        print('Boxes are incomparable')
+elif a1 >= a2 and b1 >= b2 and c1 >= c2:
+    print('The first box is larger than the second one')
+elif a1 >= a2 and b1 >= c2 and c1 >= b2:
+    print('The first box is larger than the second one')
+elif a1 >= b2 and b1 >= c2 and c1 >= a2:
+    print('The first box is larger than the second one')
 else:
-    print('Boxes are incomparable')
+    print('The first box is smaller than the second one')
