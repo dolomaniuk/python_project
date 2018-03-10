10f9502f0b8374099cbbd298f7ebc434455baa10
Dolomaniuk <dalamas200487@gmail.com>
2018-03-10 11:15:53 +0300

    100/100

--- week_2/checkers/solution.py
+++ week_2/checkers/solution.py
@@ -3,13 +3,8 @@
 
 def isEven(m, n):   # доступность выполнения скрипта
     validate = 0
-    if (1 <= m) and (m <= 8) and (1 <= n) and (n <= 8):
-        if n % 2 != 0:
-            if m % 2 != 0:
-                validate = 1
-        else:
-            if m % 2 == 0:
-                validate = 1
+    if ((m + n) % 2 == 0) and (1 <= m <= 8) and (1 <= n <= 8):
+        validate = 1
     return validate
 
 
@@ -19,22 +14,9 @@ x2 = int(input())
 y2 = int(input())
 
 if isEven(x1, y1) and isEven(x2, y2):
-    varX = x1
-    varY = y1
-
-    while varY < y2:
-        varY += 1
-        if x2 <= varX:
-            if (varX - 1) >= 1:  # что бы не выйти за края
-                varX -= 1
-        else:
-            if (varX + 1) <= 8:  # что бы не выйти за края
-                varX += 1
-        print(varX, varY)
-else:
-    varX = -x2
-
-if x2 == varX:
-    print('YES')
+    if abs(x2 - x1) <= (y2 - y1):
+        print('YES')
+    else:
+        print('NO')
 else:
     print('NO')
