701e9489002abb8633e981b5207ec2d93d38491a
Dolomaniuk <dalamas200487@gmail.com>
2018-03-13 23:21:40 +0300

    add округление

--- week_3/test.py
+++ week_3/test.py
@@ -1,2 +1,8 @@
-print('{0:.50f}'.format(0.1))
+# print('{0:.50f}'.format(0.1))
 print(float(2**100).as_integer_ratio())
+from math import floor, ceil, trunc
+print(int(2.5), int(3.5), int(-2.5))
+print(trunc(2.5), trunc(3.5), trunc(-2.5))
+print(floor(2.5), floor(3.5), floor(-2.5))
+print(ceil(2.5), ceil(3.5), ceil(-2.5))
+print(round(2.5), round(3.5), round(-2.5))
