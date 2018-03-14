fd52d668adb05fed8b2ae68a26f5013cd586fdb6
Dolomaniuk <dalamas200487@gmail.com>
2018-03-14 22:20:48 +0300

    100/100

--- week_3/цена товара/solution.py
+++ week_3/цена товара/solution.py
@@ -1,6 +1,6 @@
 n = float(input())
-n = n * 100
-rub = int(n // 100)
-coin = round(n % 1000)
 
-print(rub, '{0:02d}'.format(coin % 100))
+rub = int(n)
+coin = round((n * 100)) % 100
+
+print(rub, coin)
