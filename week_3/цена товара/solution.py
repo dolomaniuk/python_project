aefab9fd6676467cba39ac2bbfc4565f5362105d
Dolomaniuk <dalamas200487@gmail.com>
2018-03-14 21:24:46 +0300

    67/100

--- week_3/цена товара/solution.py
+++ week_3/цена товара/solution.py
@@ -1,7 +1,8 @@
+import math
 n = float(input())
 n = n * 100
 rub = int(n // 100)
-coinD = int(((n % 100) - (n % 10)) / 10)
+coinD = math.floor(((n % 100) - (n % 10)) / 10)
 coinS = int(n % 10)
 
 print(rub, str(coinD) + str(coinS))
