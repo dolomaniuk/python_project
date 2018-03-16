6d6aba6e6fccda6674078fecb90b259974a7c5b3
Dolomaniuk <dalamas200487@gmail.com>
2018-03-16 19:12:50 +0300

    100/100

--- week_3/проценты/solution.py
+++ week_3/проценты/solution.py
@@ -2,8 +2,7 @@ P = int(input())
 X = int(input())
 Y = int(input())
 
-S = X + Y / 100
-S *= 1 + P / 100
-coin = round(100 * (S - (int(S))))
-# print(100 * (S - (int(S))))
-print(int(S), coin)
+S = X * 100 + Y
+S = int(S * (100 + P) / 100)
+
+print(S // 100, S % 100)
