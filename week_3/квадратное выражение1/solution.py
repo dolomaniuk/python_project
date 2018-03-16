2d89ddb23bed8516bf09734798f90f0cf742883d
Dolomaniuk <dalamas200487@gmail.com>
2018-03-16 21:56:08 +0300

    75/100

--- week_3/квадратное выражение1/solution.py
+++ week_3/квадратное выражение1/solution.py
@@ -1,9 +1,13 @@
 from math import sqrt
-a = int(input())
-b = int(input())
-c = int(input())
+a = float(input())
+while a == 0:
+    print('a have to be > 0. Input a:')
+    a = int(input())
+b = float(input())
+c = float(input())
 
 D = b ** 2 - (4 * a * c)
+# print(D)
 if D > 0:
     maxX = (-b + sqrt(D)) / (2 * a)
     minX = (-b - sqrt(D)) / (2 * a)
