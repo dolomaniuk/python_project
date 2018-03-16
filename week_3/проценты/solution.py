2af2a981c023d8a4dfbe103fb51047449f8f5865
dolomaniuk <dalamas200487@gmail.com>
2018-03-16 09:54:31 +0300

    12/100

--- week_3/проценты/solution.py
+++ week_3/проценты/solution.py
@@ -1,3 +1,4 @@
+from math import floor
 P = int(input())
 X = int(input())
 Y = int(input())
@@ -5,5 +6,5 @@ Y = int(input())
 S = X * 100 + Y
 
 S *= 1 + P / 100
-
-print(int(S / 100), round(S % 100))
+# print(S % 100)
+print(int(S / 100), floor(S % 100))
