68a87eb6bbbd13308c4dd2f62d3314bbb76d5d63
Dolomaniuk <dalamas200487@gmail.com>
2018-03-18 22:08:43 +0300

    метод Крамера 75/100

--- week_3/СЛАУ/solution.py
+++ week_3/СЛАУ/solution.py
@@ -1,3 +1,4 @@
+# решение методом Крамера
 a = int(input())
 b = int(input())
 c = int(input())
@@ -5,39 +6,11 @@ d = int(input())
 e = int(input())
 f = int(input())
 
+delta = a * d - b * c
+delX1 = e * d - b * f
+delX2 = a * f - e * c
 
-def sokrasheniu(l, m, n):
-    m /= l
-    n /= l
-    l /= l
-    return int(l), m, n
+x1 = delX1 / delta
+x2 = delX2 / delta
 
-
-while (a != 1) or (c != 1):
-    if a == 1:
-        varzero = -c / a
-        c += varzero * a
-        d += varzero * b
-        f += varzero * e
-        # print('varzero', varzero, 'c=', c, 'd=', d, 'f=', f)
-        y = f / d
-        x = e - (b * y)
-        break
-    elif c == 1:
-        varzero = -a / c
-        a += varzero * c
-        b += varzero * d
-        e += varzero * f
-        # print('varzero=', varzero, 'a=', a, 'b=', b, 'e=', e)
-        y = e / b
-        x = f - (d * y)
-        break
-    else:
-        if a > c:
-            a, b, e = sokrasheniu(a, b, e)
-            # print('1)', a, b, c)
-        else:
-            c, d, f = sokrasheniu(c, d, f)
-            # print('2)', c, d, f)
-
-print(x, y)
+print(x1, x2)
