6f939a3276b99c8fa0bc49ce0ffe35dee92c2bec
Dolomaniuk <dalamas200487@gmail.com>
2018-03-18 21:12:16 +0300

    38/100 runtime err

--- week_3/СЛАУ/solution.py
+++ week_3/СЛАУ/solution.py
@@ -5,32 +5,39 @@ d = int(input())
 e = int(input())
 f = int(input())
 
-if a == 1:
-    varzero = -c / a
-    c += varzero * a
-    d += varzero * b
-    f += varzero * e
-    print('varzero', varzero, 'c', c, 'd', d)
-    y = f / d
-    x = e - (b * y)
-    print('y', y, 'x', x)
-elif c == 1:
-    varzero = -a / c
-    a += varzero * c
-    b += varzero * d
-    y = e / b
-    x = f - (d * y)
-else:
-    a = 1
-    b /= a
-    e /= a
-    varzero = -c / a
-    c += varzero * a
-    d += varzero * b
-    y = f / d
-    x = e - (b * y)
+
+def sokrasheniu(l, m, n):
+    m /= l
+    n /= l
+    l /= l
+    return int(l), m, n
+
+
+while (a != 1) or (c != 1):
+    if a == 1:
+        varzero = -c / a
+        c += varzero * a
+        d += varzero * b
+        f += varzero * e
+        # print('varzero', varzero, 'c=', c, 'd=', d, 'f=', f)
+        y = f / d
+        x = e - (b * y)
+        break
+    elif c == 1:
+        varzero = -a / c
+        a += varzero * c
+        b += varzero * d
+        e += varzero * f
+        # print('varzero=', varzero, 'a=', a, 'b=', b, 'e=', e)
+        y = e / b
+        x = f - (d * y)
+        break
+    else:
+        if a > c:
+            a, b, e = sokrasheniu(a, b, e)
+            # print('1)', a, b, c)
+        else:
+            c, d, f = sokrasheniu(c, d, f)
+            # print('2)', c, d, f)
 
 print(x, y)
-    # ax + by = e
-    # cx + dy = f
-
