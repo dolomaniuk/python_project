1843c0634fe0bffc93127bc89671feda0e761d62
Dolomaniuk <dalamas200487@gmail.com>
2018-03-19 19:03:09 +0300

    test string

--- week_3/test.py
+++ week_3/test.py
@@ -6,3 +6,11 @@ print(trunc(2.5), trunc(3.5), trunc(-2.5))
 print(floor(2.5), floor(3.5), floor(-2.5))
 print(ceil(2.5), ceil(3.5), ceil(-2.5))
 print(round(2.5), round(3.5), round(-2.5))
+
+s = 'abcd abc abd'
+pos = s.find('abc')
+
+while pos != -1:
+    print(pos)
+    pos = s.find('abc', pos + 1)
+
