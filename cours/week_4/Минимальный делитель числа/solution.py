b216d9b4f0f7d392a40712e16318888727e511e4
Dolomaniuk <dalamas200487@gmail.com>
2018-03-26 23:32:11 +0300

    58/100 Test 49 Time Limit Exceeded

--- cours/week_4/Минимальный делитель числа/solution.py
+++ cours/week_4/Минимальный делитель числа/solution.py
@@ -4,6 +4,8 @@ n = int(input())
 def MinDivisor(n):
     i = 2
     while i <= n and n % i != 0:
+            if i > n ** 0.5:
+                return n
             i += 1   # переход к следующему делителю
     else:    # в противном случае,
         return i     # выводится текущий делитель
