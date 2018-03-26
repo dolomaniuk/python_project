336f40fecde57a443b164afdf87f3b4cda5f0238
Dolomaniuk <dalamas200487@gmail.com>
2018-03-26 23:34:29 +0300

    100/100

--- cours/week_4/Минимальный делитель числа/solution.py
+++ cours/week_4/Минимальный делитель числа/solution.py
@@ -4,7 +4,7 @@ n = int(input())
 def MinDivisor(n):
     i = 2
     while i <= n and n % i != 0:
-            if i > n ** 0.5:
+            if i >= n ** 0.5:
                 return n
             i += 1   # переход к следующему делителю
     else:    # в противном случае,
