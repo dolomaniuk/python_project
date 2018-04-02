09079c415bd1b1b9c038833b4a18845635974eda
Dolomaniuk <dalamas200487@gmail.com>
2018-04-02 21:22:34 +0300

    test rekursia

--- OterFiles/test.py
+++ OterFiles/test.py
@@ -1,10 +1,6 @@
-def max2(a, b):
-    if a > b:
-        return a
-    return b
-
-def max4(a, b, c, d):
-    return max2(max2(a, b), max2(c, d))
-
-print(max2(max2(2, 7), max2(1, 9)))
-print(max4(1, 3, 7, 2))
+def rec():
+    n = int(input())
+    if n != 0:
+        rec()
+        print(n)
+rec(),,,m
