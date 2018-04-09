a352a9a8076286165da576a035a590e3d9674ab6
dolomaniuk <dalamas200487@gmail.com>
2018-04-09 16:57:24 +0300

    13/100 Test 3 Runtime error

--- cours/week_5/Ближайшее число/solution.py
+++ cours/week_5/Ближайшее число/solution.py
@@ -2,12 +2,13 @@ N = int(input())
 myList = list(map(int, input().split()))
 x = int(input())
 
-if myList.count(x) > 0:
-    print(x)
-else:
-    minDelta = abs(x - myList[0])
-    for i in range(1, N):
-        if abs(x - myList[i]) < minDelta:
-            minDelta = abs(x - myList[i])
-            closerNumber = myList[i]
-    print(closerNumber)
+# if myList.count(x) > 0:
+#     print(x)
+# else:
+minDelta = abs(abs(x) - myList[0])
+for i in range(1, N):
+    delta = abs(abs(x) - myList[i])
+    if delta < minDelta:
+        minDelta = delta
+        closerNumber = myList[i]
+print(closerNumber)
