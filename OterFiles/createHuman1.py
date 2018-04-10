c306b6eaed7843247fd4184b252143e9616fe8a0
dolomaniuk <dalamas200487@gmail.com>
2018-04-10 12:14:28 +0300

    new actualDataPereson

---
+++ OterFiles/createHuman1.py
@@ -0,0 +1,122 @@
+from mimesis import Person
+import string
+import random
+import pyautogui
+from datetime import date
+# pyautogui.PAUSE = 0.25
+personalData = {'lastname': 'петров',
+                'name': 'вася',
+                'surname': 'евгеньевич',
+                'gender': 'Муж.',
+                'age': ('20','04','1987'),
+                'passport': 'AB1111111',
+                'mobile': '+375291111111',
+                'email': 'CREDOSPAM@ITWORKS.BY'}
+age = []    # возраст
+
+def currentDate():
+    day = date.today().day
+    month = date.today().month
+    year = date.today().year
+    if day < 10:
+        age.append('0' + str(day))
+    else:
+        age.append(str(day))
+    if month < 10:
+        age.append('0' + str(month))
+    else:
+        age.append(str(month))
+    age.append(str(year))
+    return age
+
+
+
+# def selfData(name, lname, surname, gend, latname, latlname)
+def selfData():
+    pyautogui.doubleClick(341, 246) # set cursor on Familiya
+    pyautogui.typewrite('lastname')   # type LastName
+    pyautogui.press('tab')
+    pyautogui.typewrite('name')     # type Name
+    pyautogui.press('tab')
+    pyautogui.typewrite('ЕГОРОВИЧ')     # type surname
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('pageup')      # set default value
+    pyautogui.press('pageup')
+
+    if personalData['gender'] == 'Муж.':
+        pyautogui.press('pagedown')    # set male
+    else:
+        pyautogui.press('pagedown')    # set female
+        pyautogui.press('pagedown')
+    
+    pyautogui.press('tab')
+    pyautogui.typewrite('ILYA')     # type latin Name
+    pyautogui.press('tab')
+    pyautogui.typewrite('PUPKIN')     # type latin LastName
+    
+
+def create_personalData():    # function of creating new client
+    passport = ''
+    person = Person('ru')
+
+    # Выводим исландские мужские имена.
+    
+    lastname = person.last_name()
+    personalData['lastname'] = lastname
+    print('\nфамилия:\t', personalData['lastname'])
+
+    name = person.name()
+    personalData['name'] = name
+    print('имя:\t\t', personalData['name'])
+
+    surname = person.surname()
+    personalData['surname'] = surname
+    print('отчество:\t', personalData['surname'])
+
+    gender = person.gender()
+    personalData['gender'] = gender
+    print('пол:\t\t', personalData['gender'])
+
+    for _ in range(2):
+        varLet = random.choice(string.ascii_uppercase)
+        passport += varLet
+    passport += person.telephone('#######')
+    personalData['passport'] = passport
+    print('паспорт:\t', personalData['passport'])
+
+### birthday
+    currentDate()
+    personalData['age'] = age
+    personalData['age'][2] = str(int(personalData['age'][2]) - person.age(16, 70))
+    print('возраст:\t', personalData['age'], '\n')
+###
+
+    print('гражд-во:\t', person.get_current_locale())
+    print('Нац-сть:\t',person.nationality())
+
+### mobile
+    mobile = person.telephone('+37529#######')
+    personalData['mobile'] = mobile
+    print('телефон:\t', personalData['mobile'])
+###
+### email
+    # personalData['email'] = person.email()
+    # print('email:\t\t', personalData['email'])
+###
+    # print('проф-ия:\t', person.occupation(), '\n')
+    # print('обращение:\t', person.title())
+    # print('взгляды:\t', person.views_on())
+    # print('вера:\t\t', person.worldview(), '\n')
+    return personalData
+
+
+create_personalData()
+selfData()
+# keys = str(input('Press "Space" and next "Enter" to repeat\nor "Enter" to exit\n').split())
+# try:
+#     while keys[0] == ' ':
+#         create_personalData()
+#         keys = input('Press "Space" and next "Enter" to create new client again\nor "Enter" to exit\n')
+# except:
+#     pass
