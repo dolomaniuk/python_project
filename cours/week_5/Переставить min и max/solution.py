1c55c7c2295afb49b88048ce65846c06d5bb1c68
Dolomaniuk <dalamas200487@gmail.com>
2018-04-11 22:17:42 +0300

    100/100

--- cours/week_5/Переставить min и max/solution.py
+++ cours/week_5/Переставить min и max/solution.py
@@ -1,17 +1,19 @@
 myList = list(map(int, input().split()))
-
-minimum = myList.index(min(myList))
-maximum = myList.index(max(myList))
-if minimum < maximum:
-    newList = myList[:minimum]
-    newList.append(myList[maximum])
-    newList.extend(myList[minimum + 1:maximum])
-    newList.append(myList[minimum])
-    newList.extend(myList[maximum + 1:])
+if len(myList) > 1:
+    minimum = myList.index(min(myList))
+    maximum = myList.index(max(myList))
+    if minimum < maximum:
+        newList = myList[:minimum]
+        newList.append(myList[maximum])
+        newList.extend(myList[minimum + 1:maximum])
+        newList.append(myList[minimum])
+        newList.extend(myList[maximum + 1:])
+    else:
+        newList = myList[:maximum]
+        newList.append(myList[minimum])
+        newList.extend(myList[maximum + 1:minimum])
+        newList.append(myList[maximum])
+        newList.extend(myList[minimum + 1:])
+    print(' '.join(map(str, newList)))
 else:
-    newList = myList[:maximum]
-    newList.append(myList[minimum])
-    newList.extend(myList[maximum + 1:minimum])
-    newList.append(myList[maximum])
-    newList.extend(myList[minimum + 1:])
-print(' '.join(map(str, newList)))
+    print(myList[0])
