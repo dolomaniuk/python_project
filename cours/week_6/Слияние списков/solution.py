832b0d1d53ca19ae58bafcd550b6aa8a6d78a451
Dolomaniuk <dalamas200487@gmail.com>
2018-04-17 00:11:35 +0300

    83/100 Limit error

--- cours/week_6/Слияние списков/solution.py
+++ cours/week_6/Слияние списков/solution.py
@@ -1,25 +1,22 @@
 listA = list(map(int, input().split()))
 listB = list(map(int, input().split()))
 listC = []
-i = 0
 lenListC = len(listA) + len(listB)
+
+
 def merge(A, B):
     while len(listC) < lenListC:
-        if (A[0] is None) and (B[0] is not None):
-            print('0')
+        if (len(A) > 0) and (len(B) > 0):
             if A[0] < B[0]:
-                listC.append(A[0])
-                A.remove(A[0])
+                listC.append(A.pop(0))
             else:
-                listC.append(B[0])
-                B.remove(B[0])
-        elif A[0] is None:
-            print('1')
-            listC.append(B[0])
-        elif B[0] is None:
-            print('2')
-            listC.append(A[0])
+                listC.append(B.pop(0))
+        elif len(A) > 0:
+            listC.append(A.pop(0))
+        elif len(B) > 0:
+            listC.append(B.pop(0))
 
     return listC
 
-print(merge(listA, listB))
+
+print(*merge(listA, listB))
