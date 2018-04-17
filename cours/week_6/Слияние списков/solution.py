cb1154ad938c6a9f14a767ef420a4f0de846fbe3
Dolomaniuk <dalamas200487@gmail.com>
2018-04-17 12:19:21 +0300

    100/100

--- cours/week_6/Слияние списков/solution.py
+++ cours/week_6/Слияние списков/solution.py
@@ -1,24 +1,7 @@
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-newlist = []
+from heapq import merge
+listA = list(map(int, input().split()))
+listB = list(map(int, input().split()))
 
+listC = merge(listA, listB)
 
-def merge(list1, list2):
-    while list1 and list2:
-        if list1[0] == list2[0]:
-            newlist.append(list1.pop(0))
-            newlist.append(list2.pop(0))
-        elif list1[0] < list2[0]:
-            newlist.append(list1.pop(0))
-        else:
-            newlist.append(list2.pop(0))
-
-    if list1:
-        newlist.extend(list1)
-    if list2:
-        newlist.extend(list2)
-
-    return newlist
-
-
-print(*merge(A, B))
+print(*listC)
