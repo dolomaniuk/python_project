f546e92b0d0908238718efd315e5c2b68b0cecbd
Dolomaniuk <dalamas200487@gmail.com>
2018-04-20 00:15:29 +0300

    scetch

---
+++ cours/week_6/Гражданская оборона/solution.py
@@ -0,0 +1,36 @@
+from collections import namedtuple
+
+arrShel = []
+arrBombShel = []
+result = []
+
+# number of shelter
+n = int(input())
+# distance each of shelter in n
+distN = list(map(int, input().split()))
+for i in range(n):
+    shelter = namedtuple('Shelter', 'distance number')
+    shelter.distance = distN[i]
+    shelter.number = i
+    arrShel.append(shelter)
+# number of bombshelter
+m = int(input())
+# distance each of bombshelter in m
+distM = list(map(int, input().split()))
+for i in range(m):
+    bombshelter = namedtuple('Bombshelter', 'distance number')
+    bombshelter.distance = distM[i]
+    bombshelter.number = i
+    arrBombShel.append(bombshelter)
+
+arrShel.sort(key=lambda shelter: (shelter.distance ** 2, shelter.number))
+for now in arrShel:
+    print(now.distance, now.number)
+arrBombShel.sort(key=lambda bombshelter: (bombshelter.distance ** 2, bombshelter.number))
+for now in arrBombShel:
+    print(now.distance, now.number)
+
+for i in arrShel:
+    for j in arrBombShel:
+        result.append(tuple(abs(arrShel[i].distance - arrBombShel[j].distance), arrBombShel[j].number))
+        print(*result)
