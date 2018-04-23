3c9ef0a58a7354bb326d5e7195affd39a7f1821d
dolomaniuk <dalamas200487@gmail.com>
2018-04-23 16:57:51 +0300

    почти готово

--- cours/week_6/Гражданская оборона/solution.py
+++ cours/week_6/Гражданская оборона/solution.py
@@ -8,33 +8,30 @@ result = []
 n = int(input())
 # distance each of shelter in n
 distN = list(map(int, input().split()))
-for i in range(n):
-    shelter = namedtuple('Shelter', 'distance number')
-    shelter.distance = distN[i]
-    shelter.number = i
-    arrShel.append(shelter)
+newListOfDistN = []
+for i in range(len(distN)):
+    newListOfDistN.append((distN[i], i))
+newListOfDistN.sort()
 # number of bombshelter
 m = int(input())
 # distance each of bombshelter in m
 distM = list(map(int, input().split()))
-for i in range(m):
-    bombshelter = namedtuple('Bombshelter', 'distance number')
-    bombshelter.distance = distM[i]
-    bombshelter.number = i
-    arrBombShel.append(bombshelter)
+newListOfDistM = []
+for i in range(len(distM)):
+    newListOfDistM.append((distM[i], i))
+newListOfDistM.sort()
 
-arrShel.sort(key=lambda shelter: (shelter.distance ** 2, shelter.number))
-for now in arrShel:
-    print(now.distance, now.number)
-arrBombShel.sort(key=lambda bombshelter: (bombshelter.distance ** 2, bombshelter.number))
-for now in arrBombShel:
-    print(now.distance, now.number)
+newMinList = []
+print(newListOfDistM[0][0], '----')
+tmpMin = newListOfDistM[0][1]
 
-res = []
-for i in arrShel:
-    for j in arrBombShel:
-        res = namedtuple('Result', 'distance number')
-        res.distance = abs(int(arrShel[i].distance) - int(arrBombShel[j].distance))
-        res.number = arrBombShel[j].number
-        result.append(tuple(res.distance, res.number))
-        print(*result)
+for i in range(len(newListOfDistN)):
+    for j in range(len(newListOfDistM)):
+        tmp = abs(newListOfDistM[j][0] - newListOfDistN[i][0])
+        print(abs(newListOfDistM[j][0] - newListOfDistN[i][0]))
+        if tmp < tmpMin:
+            tmpMin = tmp
+            print('!!', newListOfDistM[j][1])
+            newMinList.append(newListOfDistM[j][1])
+
+print(newMinList)
