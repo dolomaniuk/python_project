d8cacbe1215f29a0f78db2893934204f0b6770be
dolomaniuk <dalamas200487@gmail.com>
2018-04-23 15:33:59 +0300

    сортировка подсчетом refactor

--- OterFiles/dist/test.py
+++ OterFiles/dist/test.py
@@ -1,7 +1,6 @@
-myList = list(map(int, input().split()))
-grades = [0] * 11
-for now in myList:
-    grades[now] += 1
-for grade in range(len(grades)):
-    for i in range(grades[grade]):
-        print(grade, end=' ')
+marks = map(int, input().split())
+cntMarks = [0] * 11
+for mark in marks:
+    cntMarks[mark] += 1
+for nowMark in range(11):
+    print((str(nowMark) + ' ') * cntMarks[nowMark], end='')
