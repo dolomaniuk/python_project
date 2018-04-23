5ccfe42d66e2d72e96674ae7140f27c09be6aa33
Dolomaniuk <dalamas200487@gmail.com>
2018-04-23 20:40:00 +0300

    maybe it's working

--- cours/week_6/Гражданская оборона/solution.py
+++ cours/week_6/Гражданская оборона/solution.py
@@ -7,13 +7,14 @@ from collections import namedtuple
 with open('inpData.txt', 'r', encoding='utf8') as fin:
     data = fin.readlines()
     tmpList = []
-    for i in data:
-        tmpList.append(i.strip())
+    for line in data:
+        tmpList.append(line.strip())
 
+print(tmpList)
 n = int(tmpList[0])
-distN = tmpList[1:n + 1]
+distN = list(map(int, tmpList[1:n + 1]))
 m = int(tmpList[n + 1])
-distM = tmpList[(m + 1):]
+distM = list(map(int, tmpList[n + 2:]))
 
 print(n, distN)
 print(m, distM)
@@ -25,7 +26,7 @@ print()
 # distN = list(map(int, input().split()))
 newListOfDistN = []
 for i in range(len(distN)):
-    newListOfDistN.append((int(distN[i]), i))
+    newListOfDistN.append((distN[i], i))
 # newListOfDistN.sort()
 print(newListOfDistN)
 # number of bombshelter
@@ -34,8 +35,8 @@ print(newListOfDistN)
 # distM = list(map(int, input().split()))
 newListOfDistM = []
 for i in range(len(distM)):
-    newListOfDistM.append((int(distM[i]), i))
-newListOfDistM.sort()
+    newListOfDistM.append((distM[i], i))
+# newListOfDistM.sort()
 print(newListOfDistM)
 newMinList = []
 print('tmpMin', abs(newListOfDistM[0][0] - newListOfDistN[0][0]))
@@ -49,6 +50,6 @@ for i in range(len(newListOfDistN)):
             tmpMin = tmp
             index = newListOfDistM[j][1]
             print('index', newListOfDistM[j][1])
-    newMinList.append(newListOfDistM[index][1])
+    newMinList.append(newListOfDistM[index][1] + 1)
     print('_______')
 print(*newMinList)
