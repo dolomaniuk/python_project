87d26935dd4bafa0e7dbe6e0207e9a5fbf01817e
Dolomaniuk <dalamas200487@gmail.com>
2018-04-24 00:18:44 +0300

    Test 5 Runtime error

--- cours/week_6/Гражданская оборона/solution.py
+++ cours/week_6/Гражданская оборона/solution.py
@@ -1,54 +1,41 @@
-from collections import namedtuple
-
-# arrShel = []
-# arrBombShel = []
-# result = []
-
-with open('inpData.txt', 'r', encoding='utf8') as fin:
-    data = fin.readlines()
-    tmpList = []
-    for line in data:
-        tmpList.append(line.strip())
+# with open('inpData.txt', 'r', encoding='utf8') as fin:
+#     data = fin.readlines()
+#     tmpList = []
+#     for line in data:
+#         tmpList.append(line.strip())
 
 # print(tmpList)
-n = int(tmpList[0])
-distN = list(map(int, tmpList[1].split()))
-m = int(tmpList[2])
-distM = list(map(int, tmpList[3].split()))
-
 # print(n, distN)
 # print(m, distM)
 # print()
 
 # number of shelter
-# n = int(input())
+# n = int(tmpList[0])
+n = int(input())
+
 # distance each of shelter in n
-# distN = list(map(int, input().split()))
+# distN = list(map(int, tmpList[1].split()))
+distN = list(map(int, input().split()))
 newListOfDistN = []
 for i in range(n):
     newListOfDistN.append((distN[i], i))
-# newListOfDistN.sort()
-# print(newListOfDistN)
+
 # number of bombshelter
-# m = int(input())
+# m = int(tmpList[2])
+m = int(input())
+
 # distance each of bombshelter in m
-# distM = list(map(int, input().split()))
+# distM = list(map(int, tmpList[3].split()))
+distM = list(map(int, input().split()))
 newListOfDistM = []
 for i in range(m):
     newListOfDistM.append((distM[i], i))
-# newListOfDistM.sort()
-# print(newListOfDistM)
-newMinList = []
 
-for i in range(len(newListOfDistN)):
-    tmpMin = abs(newListOfDistM[0][0] - newListOfDistN[i][0])
-    for j in range(len(newListOfDistM)):
-        tmp = abs(newListOfDistM[j][0] - newListOfDistN[i][0])
-        # print('dist', tmp, end=' ')
-        if tmp <= tmpMin:
-            tmpMin = tmp
-            index = newListOfDistM[j][1]
-            # print('index', newListOfDistM[j][1] + 1)
-    newMinList.append(newListOfDistM[index][1] + 1)
-    # print('\n_______\n')
-print(*newMinList)
+newMinList = []
+tmp = []
+for i in range(n):
+    for j in range(m):
+        tmp = abs(newListOfDistM[j][0] - newListOfDistN[i][0]), j + 1
+        newMinList.append(list(tmp))
+    print(min(newMinList, key=lambda newMinList: newMinList[0])[1], end=' ')
+    newMinList = []
