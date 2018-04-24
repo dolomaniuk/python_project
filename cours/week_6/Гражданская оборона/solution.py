369f868b6f1801483c535a8e71e4669f52841414
dolomaniuk <dalamas200487@gmail.com>
2018-04-24 16:19:12 +0300

    dev version

--- cours/week_6/Гражданская оборона/solution.py
+++ cours/week_6/Гражданская оборона/solution.py
@@ -4,11 +4,6 @@
 #     for line in data:
 #         tmpList.append(line.strip())
 
-# print(tmpList)
-# print(n, distN)
-# print(m, distM)
-# print()
-
 # number of shelter
 # n = int(tmpList[0])
 n = int(input())
@@ -16,9 +11,7 @@ n = int(input())
 # distance each of shelter in n
 # distN = list(map(int, tmpList[1].split()))
 distN = list(map(int, input().split()))
-newListOfDistN = []
-for i in range(n):
-    newListOfDistN.append((distN[i], i))
+distN.sort()
 
 # number of bombshelter
 # m = int(tmpList[2])
@@ -30,12 +23,12 @@ distM = list(map(int, input().split()))
 newListOfDistM = []
 for i in range(m):
     newListOfDistM.append((distM[i], i))
-
+newListOfDistM.sort(key=lambda new)
 newMinList = []
 tmp = []
 for i in range(n):
     for j in range(m):
-        tmp = abs(newListOfDistM[j][0] - newListOfDistN[i][0]), j + 1
+	    tmp = abs(newListOfDistM[j][0] - distN[i]), j + 1
         newMinList.append(list(tmp))
     print(min(newMinList, key=lambda newMinList: newMinList[0])[1], end=' ')
     newMinList = []
