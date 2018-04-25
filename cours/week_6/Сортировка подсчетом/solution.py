cd47bfeb455edba42551963ee8e255a887d7e891
Dolomaniuk <dalamas200487@gmail.com>
2018-04-25 20:08:41 +0300

    100/100

--- cours/week_6/Сортировка подсчетом/solution.py
+++ cours/week_6/Сортировка подсчетом/solution.py
@@ -1,12 +1,13 @@
 myList = list(map(int, input().split()))
 
+newList = [0] * 101
+
 
 def CountSort(A):
-    newList = [0] * 100
     for num in A:
         newList[num] += 1
     # print(newList)
-    for i in range(100):
+    for i in range(101):
         # if newList[i] > 0:
         print((str(i) + ' ') * newList[i], end='')
 
