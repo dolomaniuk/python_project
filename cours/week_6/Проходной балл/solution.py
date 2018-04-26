43f9be32c33491fc431b5aab7a523505247ec627
Dolomaniuk <dalamas200487@gmail.com>
2018-04-26 23:38:24 +0300

    100/100

--- cours/week_6/Проходной балл/solution.py
+++ cours/week_6/Проходной балл/solution.py
@@ -1,39 +1,29 @@
-PassExam = True
-scoreList = []      # [0] кол-во проходных учеников
+myFile = open("input.txt", "r", encoding="utf8")
+k = int(myFile.readline())
+myList = []
+for line in myFile:
+    newLine = line.split()
+    if int(newLine[-1]) >= 40 and int(newLine[-2]) >= 40 \
+            and int(newLine[-3]) >= 40:
+        myList.append(newLine)
+myFile.close()
+myList.sort(key=lambda a: int(a[-1]) + int(a[-2]) + int(a[-3]))
+myList.reverse()
+konk = []
+for i in myList:
+    sum = int(i[-1]) + int(i[-2]) + int(i[-3])
+    konk.append(sum)
+n = len(konk)
 
-with open('input.txt', 'r', encoding='utf8') as fileIn:
-    k = int(fileIn.readline())
-    scoreList.append(0)
-    for line in fileIn.readlines():
-        data = line.split()
-        egeScores = list(map(int, data[-3:]))
-        for i in egeScores:
-            if i < 40:
-                PassExam = False
-        if PassExam:
-            score = sum(egeScores)
-            scoreList[0] += 1
-            scoreList.append(score)
-        PassExam = True
 
-scoreList.sort(reverse=True)
-# print(scoreList[-1])
-with open('output.txt', 'w', encoding='utf8') as fout:
-    if scoreList.count(scoreList[0]) > k:
-        print(1, file=fout)
-    elif 0 < scoreList[-1] <= k:
-            print(0, file=fout)
-    elif scoreList[-1] == 0:
-        print('', file=fout)
-    elif scoreList.count(scoreList[0]) == k:
-        print('', file=fout)
-    else:
-        passScore = scoreList[k]
-        if scoreList[k - 1] == scoreList[k]:
-            for scr in scoreList[k - 2::-1]:
-                if scr != passScore:
-                    passScore = scr
-                    break
-                else:
-                    continue
-        print(passScore, file=fout)
+def konkurs(n, k, konk):
+    if n <= k:
+        return 0
+    elif konk[k] == konk[0]:
+        return 1
+    for i in range(k, 0, -1):
+        if konk[i] < konk[i - 1]:
+            return konk[i - 1]
+
+
+print(konkurs(n, k, konk))
