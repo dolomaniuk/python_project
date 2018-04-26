0c3dcbc7e0315e8bb3c40638c5dc00ed3403d6a0
Dolomaniuk <dalamas200487@gmail.com>
2018-04-26 23:36:53 +0300

    refactor code Test 4 Wrong answer. 4/100

--- cours/week_6/Проходной балл/solution.py
+++ cours/week_6/Проходной балл/solution.py
@@ -18,22 +18,22 @@ with open('input.txt', 'r', encoding='utf8') as fileIn:
 
 scoreList.sort(reverse=True)
 # print(scoreList[-1])
-
-if scoreList.count(scoreList[0]) > k:
-    print(1)
-elif 0 < scoreList[-1] <= k:
-        print(0)
-elif scoreList[-1] == 0:
-    print('')
-elif scoreList.count(scoreList[0]) == k:
-    print('')
-else:
-    passScore = scoreList[k]
-    if scoreList[k - 1] == scoreList[k]:
-        for scr in scoreList[k - 2::-1]:
-            if scr != passScore:
-                passScore = scr
-                break
-            else:
-                continue
-    print(passScore)
+with open('output.txt', 'w', encoding='utf8') as fout:
+    if scoreList.count(scoreList[0]) > k:
+        print(1, file=fout)
+    elif 0 < scoreList[-1] <= k:
+            print(0, file=fout)
+    elif scoreList[-1] == 0:
+        print('', file=fout)
+    elif scoreList.count(scoreList[0]) == k:
+        print('', file=fout)
+    else:
+        passScore = scoreList[k]
+        if scoreList[k - 1] == scoreList[k]:
+            for scr in scoreList[k - 2::-1]:
+                if scr != passScore:
+                    passScore = scr
+                    break
+                else:
+                    continue
+        print(passScore, file=fout)
