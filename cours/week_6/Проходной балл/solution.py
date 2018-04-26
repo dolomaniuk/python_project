4ce6eecb7cbe9650b4fb01c36b6cbf1aaf00db37
Dolomaniuk <dalamas200487@gmail.com>
2018-04-26 20:44:20 +0300

    11/100 Test 11 Wrong answer.

--- cours/week_6/Проходной балл/solution.py
+++ cours/week_6/Проходной балл/solution.py
@@ -1,39 +1,37 @@
-Pass = True
-listOfScore = []
+PassExam = True
+scoreList = []      # [0] кол-во проходных учеников
 
 with open('input.txt', 'r', encoding='utf8') as fileIn:
     k = int(fileIn.readline())
-
+    scoreList.append(0)
     for line in fileIn.readlines():
         data = line.split()
         egeScores = list(map(int, data[-3:]))
         for i in egeScores:
             if i < 40:
-                Pass = False
-        if Pass:
+                PassExam = False
+        if PassExam:
             score = sum(egeScores)
-            listOfScore.append(score)
-        Pass = True
+            scoreList[0] += 1
+            scoreList.append(score)
+        PassExam = True
 
-listOfScore.sort(reverse=True)
-# print(listOfScore)
+scoreList.sort(reverse=True)
+# print(scoreList)
 
-countMax = listOfScore.count(listOfScore[k - 1])
-lastIndexK = listOfScore.index(listOfScore[k - 1])
-# print('countMax=', countMax, 'ind', lastIndexK)
-
-if countMax >= k:
-    print(1)
-elif 0 < len(listOfScore) < k:
+if scoreList[-1] <= k:
     print(0)
-elif len(listOfScore) == 0:
-    print("")
+elif k == 0:
+    pass
+elif scoreList.count(scoreList[0]) > k:
+    print(1)
 else:
-    # pass scoring bal
-    for scr in range(lastIndexK, -1, -1):
-        if listOfScore[scr] == listOfScore[lastIndexK]:
-            continue
-        else:
-            passScore = listOfScore[scr]
-            break
+    passScore = scoreList[k - 1]
+    if scoreList[k - 1] == scoreList[k]:
+        for scr in scoreList[k - 2:0:-1]:
+            if scr != passScore:
+                passScore = scr
+                break
+            else:
+                continue
     print(passScore)
