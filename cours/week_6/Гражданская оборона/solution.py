8659277f4c5795730d758db01d5a11af86d2d372
Dolomaniuk <dalamas200487@gmail.com>
2018-04-27 23:46:59 +0300

    new code

--- cours/week_6/Гражданская оборона/solution.py
+++ cours/week_6/Гражданская оборона/solution.py
@@ -1,55 +1,56 @@
-with open('inpData.txt', 'r', encoding='utf8') as fin:
-    data = fin.readlines()
-    tmpList = []
-    for line in data:
-        tmpList.append(line.strip())
+def minDistance(numberOfListN, valueOfNewListM, index):
+    listDist = []
+    result = []
+    count = 0
+    # print('index', index)
+    # print(valueOfNewListM[index:])
+    for ind in valueOfNewListM[index:]:
+        tmp = abs(numberOfListN[0] - ind[0]), ind[1], count
+        listDist.append(tmp)
+        count += 1
+    # print('listDist', listDist)
+    result = min(listDist, key=lambda p: p[0])
+    # print('result[', numberOfListN[1], '] -->', result[2])
 
-# n = int(input())
-# distN = list(map(int, input().split()))
-# m = int(input())
-# distM = list(map(int, input().split()))
-
-n = int(tmpList[0])
-distN = list(map(int, tmpList[1].split()))
-m = int(tmpList[2])
-distM = list(map(int, tmpList[3].split()))
-
-# print(tmpList)
-# print(n, distN)
-# print(m, distM)
-# print()
-
-# number of shelter
+    return numberOfListN[1], result[1] + 1, result[2]
 
 
+# with open('inpData.txt', 'r', encoding='utf8') as fin:
+#     data = fin.readlines()
+#     tmpList = []
+#     for line in data:
+#         tmpList.append(line.strip())
 
-# distance each of shelter in n
-# number of bombshelter
-# distance each of bombshelter in m
+n = int(input())
+listN = list(map(int, input().split()))
+m = int(input())
+listM = list(map(int, input().split()))
 
-newListOfDistM = []
-for i in range(m):
-    newListOfDistM.append((distM[i], i))
-sortDistM = sorted(newListOfDistM, key=lambda newListOfDistM:newListOfDistM[0])
+# n = int(tmpList[0])
+# listN = list(map(int, tmpList[1].split()))
+# m = int(tmpList[2])
+# listM = list(map(int, tmpList[3].split()))
 
-newMinList = []
-tmp = []
-
-x = []
-
+output = []
+output = [0] * n
+newListN = []
 for i in range(n):
-    tmp1 = abs(sortDistM[0][0] - distN[i]), 1
-    for j in range(m):
-        tmp = abs(sortDistM[j][0] - distN[i]), j + 1
-        if tmp > tmp1:
-            print('dist1', newListOfDistM[j - 1][0])
-            tmp = tmp1
-        elif tmp == tmp1:
-            print('dist2', newListOfDistM[j - 1][0])
-            # newMinList.append(list(tmp))
-        else:
-            continue
-    newMinList.append(list(tmp))
-    x = min(newMinList, key=lambda newMinList: newMinList[0])[1]
-    print(x)
-    newMinList = []
+    newListN.append([listN[i], i])
+newListN.sort(key=lambda pos: pos[0])
+
+newListM = []
+for i in range(m):
+    newListM.append([listM[i], i])
+newListM.sort(key=lambda pos: pos[0])
+
+i = 0
+for each in newListN:
+    # print('each', each)
+    index, value, i = minDistance(each, newListM, i)
+    output[index] = value
+    if i > 2:
+        i -= 1
+    # print('i=', i)
+
+for i in output:
+    print(i, end=' ')
