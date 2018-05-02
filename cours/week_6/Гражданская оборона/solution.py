bf585eac95e6b337a3641467231ea056332fbd1a
Dolomaniuk <dalamas200487@gmail.com>
2018-05-02 23:55:18 +0300

    new refactor code Test 5 Time Limit Exceeded

--- cours/week_6/Гражданская оборона/solution.py
+++ cours/week_6/Гражданская оборона/solution.py
@@ -1,35 +1,18 @@
-# def minDistance(numberOfListN, valueOfNewListM, index):
-#     listDist = []
-#     result = []
-#     count = 0
-#     # print('index', index)
-#     # print(valueOfNewListM[index:])
-#     for ind in valueOfNewListM[index:]:
-#         tmp = abs(numberOfListN[0] - ind[0]), ind[1], count
-#         listDist.append(tmp)
-#         count += 1
-#     # print('listDist', listDist)
-#     result = min(listDist, key=lambda p: p[0])
-#     # print('result[', numberOfListN[1], '] -->', result[2])
-#
-#     return numberOfListN[1], result[1] + 1, result[2]
-#
-#
-# with open('inpData.txt', 'r', encoding='utf8') as fin:
-#     data = fin.readlines()
-#     tmpList = []
-#     for line in data:
-#         tmpList.append(line.strip())
+# n = int(input())
+# listN = list(map(int, input().split()))
+# m = int(input())
+# listM = list(map(int, input().split()))
 
-n = int(input())
-listN = list(map(int, input().split()))
-m = int(input())
-listM = list(map(int, input().split()))
+with open('inpData.txt', 'r', encoding='utf8') as fin:
+    data = fin.readlines()
+    tmpList = []
+    for line in data:
+        tmpList.append(line.strip())
 
-# n = int(tmpList[0])
-# listN = list(map(int, tmpList[1].split()))
-# m = int(tmpList[2])
-# listM = list(map(int, tmpList[3].split()))
+n = int(tmpList[0])
+listN = tuple(map(int, tmpList[1].split()))
+m = int(tmpList[2])
+listM = tuple(map(int, tmpList[3].split()))
 
 if m == 1:
     for each in listN:
@@ -48,25 +31,25 @@ elif m > 1:
     newListM.sort(key=lambda pos: pos[0])
     j = 0
     for eachN in newListN:
-        print('____eachN____', eachN)
-        print(newListM)
-        distFirst = abs(eachN[0] - newListM[j][0])
-        print('j=', j, 'distF=', distFirst)
-        # index, value, i = minDistance(each, newListM, i)
-        print('srez', newListM[j:])
+        # print('____eachN____', eachN)
+        # print(newListM)
+        distFirst = 100000
+        # print('j=', j, 'distF=', distFirst)
+        # print('srez', newListM[j:])
         for jM in newListM[j:]:
-            print('jM', jM)
+            # print('jM', jM)
             distSec = abs(eachN[0] - jM[0])
-            print('distSec', distSec)
-            if distSec <= distFirst:
+            # print('distSec', distSec)
+            if distSec < distFirst:
                 output[eachN[1]] = jM[1] + 1
-                print('output[', eachN[1], ']=', output[eachN[1]], '!@!#!@@#')
+                # print('output[', eachN[1], ']=', output[eachN[1]])
                 j = newListM.index(jM)
-                print('index:', j + 1)
+                # print('index:', j)
                 distFirst = distSec
+                # print('distSF=', distFirst)
                 continue
             else:
                 break
-    print(output)
+    print(*output)
 else:
     pass
