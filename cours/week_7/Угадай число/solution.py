39d230198b55278bdc2c50960e8e31bc1cf9c3c5
Dolomaniuk <dalamas200487@gmail.com>
2018-06-07 23:07:39 +0300

    100/100 with comments

--- cours/week_7/Угадай число/solution.py
+++ cours/week_7/Угадай число/solution.py
@@ -1,24 +1,27 @@
-from random import randint
-
+# вводим лимит чисел
 n = int(input())
-aug_number = randint(1, n)
-# print(aug_number)
-answers = []
-inp_str = input()
-while inp_str.upper() != 'HELP' and not inp_str.isalpha():
-    b_answer = list(map(int, inp_str.split()))
-    for num in b_answer:
-        answers.append(num)
-    if aug_number in b_answer:
-        print('YES')
-        if b_answer[0] == aug_number and len(b_answer) == 1:
-            for i in answers:
-                if i == b_answer[0]:
-                    answers.remove(i)
-            break
-    else:
-        print('NO')
-    inp_str = input()
+# образуем множество доступных ответов
+valid_answer = set(range(1, n + 1))
 
-result = set(range(1, n + 1)) - set(answers)
-print(*sorted(result))
+beatreace_message = ''  #ответ Биатрис
+while beatreace_message.upper() != 'HELP':  #пока Биатрис не просит хелп
+    beatreace_message = input()     #ответ Биатрис
+    try:
+        #преобразуем ответ из чисел в множество
+        attempt_request = set(map(int, beatreace_message.split()))
+        #ответ Августа, есть ли в ответе Биатрис загаданное число
+        answer = str(input())
+        if answer.upper() == 'NO':
+            #вычитаем из возможных ответов, числа Биатрис
+            #оставляем уникальные значения в множестве возм. ответов
+            valid_answer -= attempt_request
+        elif answer.upper() == 'YES':
+            #обрезаем множество возм.значений до значений чисел Биатрис
+            #пересечение множеств
+            valid_answer &= attempt_request
+            #если точно назвала цифру
+            if len(attempt_request) == 1:
+                print(beatreace_message)
+                break
+    except ValueError:      #ответ Биатрис был не числом, а просьбой хелп
+        print(*sorted(valid_answer))
