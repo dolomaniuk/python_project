e77b92e1d9c300f1890a3cc7608b4932af3d3200
Dolomaniuk <dalamas200487@gmail.com>
2018-08-08 23:14:28 +0300

    Test 21 Time Limit Exceeded

--- cours/week_7/Номер появления слова/solution.py
+++ cours/week_7/Номер появления слова/solution.py
@@ -1,21 +1,7 @@
-listWords = {}  # словарь в виде номер слова: (слово: кол-во раз встречается)
-tmp = {}
-count = 1
-result = []
-with open('input.txt') as fileIn:
-    text = fileIn.readlines()
-    for string in text:
-        for word in string.split():
-            listWords[count] = {word: 0}
-            count += 1
-for count in listWords.keys():
-    key = list(listWords[count].items())
-    # (key[0][0]) - сам ключ в словаре под номером count
-    if key[0][0] in tmp:
-        tmp[key[0][0]] += 1  # увеличиваем счетчик появление слова
-        listWords[count][key[0][0]] = tmp[key[0][0]]
-    else:
-        tmp[key[0][0]] = 0    # добавляем новое слово в словарь
-    result.append(tmp[key[0][0]])
-# print(*listWords.values(), sep='\n')
-print(*result)
+with open('input.txt') as inf:
+    count = {}
+    for line in inf:
+        words = list(line.strip().split())
+        for word in words:
+            count[word] = count.get(word, 0) + 1
+            print(int(count[word] - 1), end=" ")
